3da87c899fa0b84db568d510b52a9c0d28404295
Lan Anh <lananh@MBP-cua-Lan.localdomain>
2019-02-15 13:48:25 +0700

    file

---
+++ resources/views/login.blade.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login</title>
+</head>
+<body>
+<h1>Dang nhap</h1>
+<form action="/login" method="post">
+    <p>@csrf</p>
+    <p>
+        <input type="text" name="username" placeholder="Ten dang nhap">
+    </p>
+    <p>
+        <input type="password" name="password" placeholder="Mat khau">
+    </p>
+    <p>
+        <button type="submit">Dang nhap</button>
+    </p>
+</form>
+</body>
+</html>
